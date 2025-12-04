473ffdf24879b2234f82f6574680900a2d1882db
Thomas Baillie <tomb411.w@gmail.com>
2025-12-04 18:38:40 +1100

    Added menu, main loop, etc.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
-use std::fs;
 use clap::Parser;
 use flexi_logger::Logger;
+use std::fs;
 // use flexi_logger::{Duplicate, FileSpec, Logger};
 
 #[derive(Parser, Debug)]
 struct Args {
     // Specify the todo file path.
-    #[clap(short, long, default_value = "todo.txt", help = "Path to the todo file")]
+    #[clap(
+        short,
+        long,
+        default_value = "todo.txt",
+        help = "Path to the todo file"
+    )]
     file: String,
+}
+
+fn print_menu() {
+    println!("1. View todo list");
+    println!("2. Add a new todo");
+    println!("3. Remove a todo");
+    println!("4. Exit");
 }
 
 fn print_todo_list(contents: &str) {
@@ -24,10 +36,10 @@ fn print_todo_list(contents: &str) {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Initialize the logger.
     Logger::try_with_env_or_str("debug")?
-    // .log_to_file(FileSpec::default())         // write logs to file
-    // .duplicate_to_stderr(Duplicate::Warn)     // print warnings and errors also to the console
-    .format(flexi_logger::colored_detailed_format) // use detailed format
-    .start()?;
+        // .log_to_file(FileSpec::default())         // write logs to file
+        // .duplicate_to_stderr(Duplicate::Warn)     // print warnings and errors also to the console
+        .format(flexi_logger::colored_detailed_format) // use detailed format
+        .start()?;
 
     // Parse command line arguments.
     let args = Args::parse();
@@ -47,7 +59,26 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     print_todo_list(&contents);
 
-    fs::write(&filepath, contents)?;
+    loop {
+        print_menu();
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input)?;
+        let selection: u32 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        match selection {
+            1 => println!("You selected 1"),
+            2 => println!("You selected 2"),
+            3 => println!("You selected 3"),
+            4 => {
+                println!("Exiting...");
+                break;
+            }
+            _ => continue,
+        }
+    }
 
+    fs::write(&filepath, &contents)?;
     Ok(())
 }
